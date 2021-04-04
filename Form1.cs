428bad908958df4786c62151bb26d20cbd690842
김호기 <ghrl1005@naver.com>
2021-04-04 11:45:30 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Satisfactory_map
+{
+    public partial class Form1 : Form
+    {
+        public DirectoryInfo dirinfo;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\..\\Local\\Factorygame\\Saved\\SaveGames\\"))
+            {
+                System.Diagnostics.Process a = new System.Diagnostics.Process();
+                a.StartInfo.FileName = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\..\\Local\\Factorygame\\Saved\\SaveGames\\";
+                a.Start();
+            }
+            else
+            {
+                MessageBox.Show("세이브 파일이 없습니다. \n 실행후 \"새로만들기\" 를 한 후 저장 하여 주세요");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("맵 로딩 할때는 \"CLICK/DROP YOURSAVE GAME HERE\" 을 클릭 한후 로딩하세요");
+            Process b = new Process();
+            b.StartInfo.FileName = "https://satisfactory-calculator.com/ko/interactive-map";
+            b.Start();
+            b.WaitForExit(100);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 m = new Form2();
+            m.ShowDialog();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Satisfactory_map
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process a = new Process();
+            a.StartInfo.FileName = "https://drive.google.com/drive/my-drive";
+            a.Start();
+            a.WaitForExit(100);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process b = new Process();
+            b.StartInfo.FileName = "https://mybox.naver.com/";
+            b.Start();
+            b.WaitForExit(100);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process c = new Process();
+            c.StartInfo.FileName = "https://www.dropbox.com/home";
+            c.Start();
+            c.WaitForExit(100);
+        }
+    }
+}
